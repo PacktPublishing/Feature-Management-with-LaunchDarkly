189c8cb1a802e27cb725d88645b1c33d318ff606
Michael Gillett <michael.gillett@outlook.com>
2021-10-29 16:37:29 +0100

    Add chapter 5 details

---
+++ Chapter 5/Complete Web Application/Chapter 5 - Complete Web Application/Pages/Index.cshtml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using LaunchDarkly.Client;
+using Microsoft.AspNetCore.Http;
+using System.Diagnostics;
+
+namespace Chapter_5___Complete_Web_Application.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private ILdClient _ldClient;
+
+        // USED FOR THE FIRST EXAMPLE
+        public string CtaLabel;
+
+        public IndexModel(ILogger<IndexModel> logger, ILdClient ldClient)
+        {
+            _logger = logger;
+            _ldClient = ldClient;
+        }
+
+        public void OnGet(string countryOverride)
+        {
+            // USED FOR THE SECOND EXAMPLE
+            //var watch = new Stopwatch();
+            //watch.Start();
+
+            string country = string.IsNullOrEmpty(countryOverride) ? null : countryOverride;
+
+            User user = GetUser(country);
+
+            // USED FOR THE FIRST EXAMPLE
+             CtaLabel = _ldClient.BoolVariation("registration-experiment", user, false) ? "Join today" : "Register";
+
+            // USED FOR THE SECOND EXAMPLE
+            //if (!_ldClient.BoolVariation("registration-experiment", user, false))
+            //{
+            //    System.Threading.Thread.Sleep(5000);
+            //}
+            //else
+            //{ 
+            //    // do nothing
+            //}
+
+            // USED FOR THE SECOND EXAMPLE
+            //watch.Stop();
+            //double metric = Convert.ToDouble(watch.ElapsedMilliseconds);
+            //_ldClient.Track("page load time", user, LdValue.Null, metric);
+        }
+
+        private User GetUser(string country)
+        {
+            string ldHashCookieKey = "_ldhash";
+            string guid;
+
+            if (!Request.Cookies.ContainsKey(ldHashCookieKey))
+            {
+                guid = Guid.NewGuid().ToString();
+                var cookieOptions = new CookieOptions()
+                {
+                    Path = "/",
+                    Expires = DateTimeOffset.UtcNow.AddDays(365),
+                    IsEssential = true,
+                    HttpOnly = false,
+                    Secure = false,
+                };
+
+                Response.Cookies.Append(ldHashCookieKey, guid, cookieOptions);
+            }
+            else
+            {
+                Request.Cookies.TryGetValue(ldHashCookieKey, out guid);
+            }
+
+            return LaunchDarkly.Client.User.Builder(guid)
+                    .Country(country)
+                    .FirstName("Michael")
+                    .LastName("Gillett")
+                    .Anonymous(false)
+                    .Build();
+        }
+    }
+}
--- Chapter 5/README.md
+++ Chapter 5/README.md
@@ -4,11 +4,6 @@
 
 The Chapter 5 examples were created with Visual Studio 2019 on a Windows PC and in C# and .NET 5. The samples have been tested in Visual Studio 2019 and Visual Studio Code on a Windows PC.
 
-### Blank Web Application
-
-Use this code sample to follow along with the instructions in the chapter. You can immediately run this sample application
-
-
 ### Complete Web Application
 
 Use this code sample to see the completed project working as expected. You will need to open the Startup.cs file and navigate to line 28: 
@@ -19,3 +14,8 @@ services.AddSingleton<ILdClient>(new LdClient("YOUR_SDK_KEY"));
 
 You need to replace _YOUR_SDK_KEY_ with the LaunchDarkly SDK for your account.
 
+- If using __Visual Studio 2019__ open the _Feature-Management-with-LaunchDarkly\Chapter 5\Complete Web Application_ directory and run the application.
+
+- If using __Visual Studio Code__ open the _Feature-Management-with-LaunchDarkly\Chapter 5\Complete Web Application\Chapter 5 - Complete Web Application_ directory and run the application.
+
+The changes detailed in Chapter 5 are found in the Pages/Index.cs and Pages/Index.cshtml files.
